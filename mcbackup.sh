a79adc3a7b54d9098582ace294dde124db981ced
cyokozai <yuu4suke27@outlook.jp>
2024-09-07 03:26:32 +0900

    chore: Update mcbackup.sh script
    This commit updates the mcbackup.sh script by adding new functionalities such as copying files, compressing files, deleting old files, and sending a backup report via email. It also installs necessary packages like tar, mailutils, and curl. The script now uses the current date and time to create backup files and logs the backup process in the mcbackup.log file.

--- mcbackup.sh
+++ mcbackup.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
-readonly LOG_FILE='./minecraft/data/logs/mcbackup.log'
 STATUS=0
-cd /home/*
 
 set -e
 set -o pipefail
 
+readonly datestr=$(date '+%Y%m%d%H%M%S')
+readonly DATA_DIR='minecraft/data'
+readonly BACKUP_DIR='minecraft/backups'
+readonly LOG_FILE='minecraft/logs/mcbackup.log'
+LOG_MSG=''
+
+function log() {
+    local level="$1"
+
+    echo "${datestr} [$level] $*" >> $LOG_FILE
+    echo "${datestr} [$level] $*" >> $LOG_MSG
+
+    return
+}
+
 function copyfile() {
     local src="$1"
     local dest="$2"
-    if [ -d "$src" ]; then
-        cp "$src" "$dest"
+    local user="$3"
+    
+    if [ -d "$src" ]; 
+    then
+        eval "scp -i ~/.ssh/ed25519 -p 22 $user@denkettle:~/$src ~/$dest/backup-${datestr}"
     else
         log ERROR "File $src does not exist"
         STATUS=1
@@ -19,18 +35,70 @@ function copyfile() {
     return
 }
 
-function log() {
-    local level="$1"
-    shift
-    echo "$(date '+%Y-%m-%d %H:%M:%S') [$level] $*" >>$LOG_FILE
+function compressfile() {
+    local src="$1"
+
+    eval "tar czf /backup/backup-${datestr}.tar.gz $src/backup-${datestr}"
+
+    if [ $? -ne 0 ]
+    then
+        log ERROR "Failed to compress $src"
+        STATUS=1
+    fi
 
     return
 }
 
-if [ ! -d "$LOG_FILE" ]; then
-    touch "$LOG_FILE"
-    log INFO "Starting backup"
+function deletefile(){
+    CNT=0
+
+    for file in `ls -1t ${1}*`
+    do
+        CNT=$((CNT+1))
+            
+        if [ ${CNT} -le 10 ]
+        then
+            continue
+        fi
+
+        eval "rm ${file}"
+    done
+
+    return
+}
+
+# Main
+if [ ! -d "${LOG_FILE}" ]; 
+then
+    touch "${LOG_FILE}"
+    sudo apt -y install tar
+    sudo apt -y install mailutils
+    sudo apt -y install curl
 fi
 
 log INFO "Starting backup"
+cd
 
+# Copy files
+copyfile DATA_DIR BACKUP_DIR 'cyokozai'
+
+# Compress files
+compressfile BACKUP_DIR
+
+# Delete old files
+deletefile "${BACKUP_DIR}/backup-"
+
+# Send mail
+log INFO "Backup finished with status ${STATUS}"
+
+if [ ${STATUS} -ne 0 ]
+then
+    SUBJECT=$(echo -e "[ERROR] cms backup report")
+else
+    SUBJECT=$(echo -e "[INFO] cms backup report")
+fi
+
+echo "$(cat ${LOG_MSG})" | mailx -s "${SUBJECT}" "yourmailaddress"
+
+# Exit with status
+exit ${STATUS}
